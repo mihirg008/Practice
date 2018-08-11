a7a5fffdb0a5f528340129b8a9eb4384b31fb5ea
MG08 <32246337+mihirg008@users.noreply.github.com>
2018-08-11 14:53:07 +0530

    major update, contact div changed

---
+++ Summer-Project-1/js/cars.js
@@ -0,0 +1,50 @@
+var details_show = document.getElementById("text-data");
+var btn1 = document.getElementById("text");
+
+btn1.addEventListener("click", function() {var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.onload = function() {
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+      console.log("We connected to the server, but it returned an error.");
+    }
+
+  };
+
+  ourRequest.onerror = function() {
+    console.log("Connection error");
+  };
+});
+
+function renderHTML(data) {
+  var htmlString = "";
+
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+
+    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+      if (ii == 0) {
+        htmlString += data[i].foods.likes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.likes[ii];
+      }
+    }
+
+    htmlString += ' and dislikes ';
+
+    for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+      if (ii == 0) {
+        htmlString += data[i].foods.dislikes[ii];
+      } else {
+        htmlString += " and " + data[i].foods.dislikes[ii];
+      }
+    }
+
+    htmlString += '.</p>';
+
+  }
+
+  details_show.insertAdjacentHTML('beforeend', htmlString);
+}
--- Summer-Project-1/js/main.js
+++ Summer-Project-1/js/main.js
@@ -1,3 +1,7 @@
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
 $(window).scroll(function() {
   if ($(window).scrollTop() > 100) {
     $('nav').css('height','75px');
@@ -5,11 +9,11 @@ $(window).scroll(function() {
     $('nav').css('height','100px');
   }
 });
-
+if (w>500){
 $(window).scroll(function () {
 if ($(window).scrollTop() >= 50) {
 $('nav').css('background','black');
 } else {
 $('nav').css('background','transparent');
 }
-});
+});}
